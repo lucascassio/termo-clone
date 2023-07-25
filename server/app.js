e681d490d818054709f9764b7ff7ee08ce25b834
Lucas Cassio Costa <lucascassio@Lucass-MacBook-Air.local>
2023-07-25 12:43:06 -0300

    changing directories

--- server/app.js
+++ server/app.js
@@ -1,11 +1,7 @@
-// app.js
+
 const express = require('express');
 const app = express();
 const { getAllWords } = require('./word');
-
-// Rest of your application logic...
-
-// Route to fetch all words from the database
 app.get('/words', async (req, res) => {
   try {
     const words = await getAllWords();
